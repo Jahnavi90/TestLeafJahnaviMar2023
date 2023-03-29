2171fdab626357bbf0216ca657b23f0073eef1c0
Jahnavi90 <121550263+Jahnavi90@users.noreply.github.com>
2023-03-29 17:23:37 +0530

    Add files via upload

---
+++ day1/assignment1/Car.java
@@ -0,0 +1,24 @@
+package day1.assignment1;
+
+public class Car {
+	
+	public void driveCar() {
+		System.out.println("I drive a Car.");
+	}
+	public void applyBrake() {
+		System.out.println("I apply brakes when required.");
+	}
+	public void soundHorn() {
+		System.out.println("I sound the Horn when required.");
+	}
+	public void isPuncture() {
+		System.out.println("Did my car's front tyre get punctured?");
+	}
+	public static void main(String[] args) {
+		Car assignment1 = new Car();
+		assignment1.driveCar();
+		assignment1.applyBrake();
+		assignment1.soundHorn();
+		assignment1.isPuncture();
+	}
+}
