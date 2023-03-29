05899972ab65a0505ecb7e8a21b89b1164f70ddc
Jahnavi90 <121550263+Jahnavi90@users.noreply.github.com>
2023-03-29 18:03:14 +0530

    Add files via upload

---
+++ day1/assignments/IPhone.java
@@ -0,0 +1,10 @@
+package day1.assignments;
+
+public class IPhone {
+	public void makeCall() {
+System.out.println("I make calls with my iPhone");
+}
+	public void model() {
+		System.out.println("My iPhone model is 14Pro");
+	}
+}
---
+++ day1/assignments/SmartPhone.java
@@ -0,0 +1,11 @@
+package day1.assignments;
+
+public class SmartPhone {
+
+	public static void main(String[] args) {
+		IPhone mob = new IPhone();
+		mob.makeCall();
+		mob.model();
+	}
+
+}
