64ee723fb39a40c8f74d182307182093afb946a8
Jahnavi90 <121550263+Jahnavi90@users.noreply.github.com>
2023-04-27 16:41:46 +0530

    Update Runner_CreateMulData.java

--- day18/Runner_CreateMulData.java
+++ day18/Runner_CreateMulData.java
@@ -5,8 +5,10 @@ import stepDefinition.BaseClass;
 
 @CucumberOptions(features = "src/test/java/features/CreateLeadMultipleData.feature", 
 					glue = "stepDefinition", 
-					monochrome = true, publish = true)
+					monochrome = true, publish = true,
+					tags = "@Postive or @Negative")
 
 public class Runner_CreateMulData extends BaseClass {
 
 }
+
